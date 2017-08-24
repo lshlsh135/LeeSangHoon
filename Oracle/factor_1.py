950dded3ee3d671d1db0fe91efa026bd183a2bdf
lshlsh135 <lshlsh135@naver.com>
2017-08-24 15:53:59 +0900

    코스피+코스닥 _ 모든팩터 combination 코드 만드는중 class 이용하니깐 편하다

---
+++ Oracle/factor_1.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 15:18:59 2017
+
+@author: SH-NoteBook
+"""
+
+
+import numpy as np
+import pandas as pd
+class factor_1:
+ 
+    def __init__(self,raw_data,rebalancing_date,month_date,col_num):
+        self.raw_data = raw_data
+        self.rebalancing_date = rebalancing_date
+        self.month_date = month_date
+        self.col_num = col_num
+        
+    def per(self):
+        col_length = len(self.rebalancing_date)-1 #rebalancing_date의 길이는 66이다. range로 이렇게 하면 0부터 65까지 66개의 i 가 만들어진다. -1을 해준건 실제 수익률은 -1개가 생성되기 때문.
+
+        group_goal = 5  #몇개의 그룹으로 나눌지 설정한다.
+        
+        for i in range(group_goal):
+            locals()['data_{}'.format(i)] = pd.DataFrame(np.zeros((200,col_length)))
+        
+        for i in range(group_goal):
+            locals()['data_name_{}'.format(i)] = pd.DataFrame(np.zeros((200,col_length)))
+            
+        
+        return_data = pd.DataFrame(np.zeros((group_goal,col_length)))
+        return_final = pd.DataFrame(np.zeros((group_goal,1)))
+        return_month_data = pd.DataFrame(np.zeros((1,3*col_length)))
+        
+        for n in range(col_length): 
+            first_data = self.raw_data[self.raw_data['TRD_DATE']==self.rebalancing_date.iloc[n,0]] # rebalanging할 날짜에 들어있는 모든 db data를 받아온다.
+            target_data = self.raw_data[self.raw_data['TRD_DATE']==self.rebalancing_date.iloc[n+1,0]]
+            target_data = target_data.loc[:,['TRD_DATE','GICODE','ADJ_PRC']]
+            first_data = first_data[(first_data['CAP_SIZE']==1)|(first_data['CAP_SIZE']==2)|(first_data['CAP_SIZE']==3)|(first_data['ISKOSDAQ']=='KOSDAQ')]
+            #first_data = first_data[first_data['EQUITY'].notnull()] # 처음 받아온 전체 data 에서 equity가 없는 종목은 제외한다 -> equity가 null값이라는건 저 당시에 데이타가 존재하지 않는다는 것.
+            
+            first_data = first_data[first_data.iloc[:,self.col_num].notnull()] # 1/PER이 NULL인걸 제외한다. EQUITY 가 NULL인걸 미리 제거하지 않아도 여기서 제거됨.
+            data_length = len(first_data) # 몇개의 종목이 rebalanging_date때 존재했는지 본다.
+            #월별 수익률을 구하기 위해 month_date 에서 필요한 날짜가 몇번쨰 row에 있는지 확인
+            past_month=self.month_date.loc[self.month_date['MONTH_DATE']==self.rebalancing_date.iloc[n,0]].index[0]
+            cur_month=self.month_date.loc[self.month_date['MONTH_DATE']==self.rebalancing_date.iloc[n+1,0]].index[0]
+            
+            
+            first_data=first_data.assign(rnk=np.floor(first_data.iloc[:,self.col_num].rank(method='first')/(data_length/group_goal+1/group_goal)))          # rnk가 클수록 저평가
+            
+            sum_data = pd.merge(target_data,first_data,on='GICODE') # 3개월치 수익률을 구하기 위해 3개월 후 존재하는 data에 현재 data를 붙임
+            sum_data['3M_RETURN'] = sum_data['ADJ_PRC_x']/sum_data['ADJ_PRC_y'] # 3개월동안의 종목 수익률
+            
+            first_data = first_data.loc[:,['TRD_DATE','GICODE','ADJ_PRC']]
+            for i in range(past_month+1,cur_month): # 3개월치의 월별 수익률을 구하기 위해선 4개의 price 데이터가 필요한데 2개밖에 없으니 2개를 더 받아온다.
+                second_data = self.raw_data[self.raw_data['TRD_DATE']==self.month_date.iloc[i,0]]  #월별 데이터를 받아와서
+                second_data = second_data.loc[:,['TRD_DATE','GICODE','ADJ_PRC']]   # 간단하게 만든다음
+                first_data = pd.merge(first_data,second_data,on='GICODE')   # first_data와 합친다
+            
+            first_data['1ST_RETURN'] =  first_data['ADJ_PRC_y']/ first_data['ADJ_PRC_x']   #0->1 , 즉 첫 한개월간의 수익률
+            first_data['2ND_RETURN'] =  first_data['ADJ_PRC']/ first_data['ADJ_PRC_y']# 1->2 한달이후 한달간 수익률
+            first_data = first_data.loc[:,['GICODE','1ST_RETURN','2ND_RETURN']] #데이터를 간단하게 만들어준다음
+            sum_data = pd.merge(sum_data,first_data,on='GICODE') # 기존 data와 합친다.
+            sum_data['2M_CUM_RETURN'] = sum_data['1ST_RETURN'] * sum_data['2ND_RETURN'] 
+            
+           
+            #숫자가 클수록 1/PER 가 큰거니 저평가
+            for i in range(group_goal):
+                locals()['data_{}'.format(i)] = sum_data[sum_data['rnk']==i]
+               
+            for i in range(group_goal):
+                locals()['data_name_{}'.format(i)][n] = locals()['data_{}'.format(i)]['CO_NM'].reset_index(drop=True)
+                
+            for i in range(group_goal):
+                return_data.iloc[i,n] = np.mean(locals()['data_{}'.format(i)]['3M_RETURN'])
+                
+#            for i in range(group_goal):
+#                return_month_data.iloc[i,3*n]= np.mean(locals()['data_{}'.format(i)]['1ST_RETURN'])
+#                return_month_data.iloc[i,3*n+1]= np.mean(locals()['data_{}'.format(i)]['2M_CUM_RETURN']) / np.mean(locals()['data_{}'.format(i)]['1ST_RETURN'])
+#                return_month_data.iloc[i,3*n+2]= np.mean(locals()['data_{}'.format(i)]['3M_RETURN']) / np.mean(locals()['data_{}'.format(i)]['2M_CUM_RETURN'])
+#                
+#               
+        
+            return_final=np.product(return_data,axis=1)
+        return [return_final, return_data]  # 이렇게 return 하면 list로 받아짐
+                
+        
---
+++ Oracle/코스피코스닥_모든팩터조합.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 13:27:10 2017
+
+@author: SH-NoteBook
+"""
+
+
+import pandas as pd
+import numpy as np
+import cx_Oracle
+from factor_1 import factor_1
+
+
+#이거 두개 반드시 선언!
+cx0=cx_Oracle.makedsn("localhost",1521,"xe")
+connection = cx_Oracle.connect("lshlsh135","2tkdgns2",cx0) #이게 실행이 안될때가 있는데
+#그때는 services에 들어가서 oracle listner를 실행시켜줘야함
+
+kospi_quarter = pd.read_pickle('kospi_quarter')
+#DATA를 가져온다!!
+kospi = pd.read_sql("""select * from kospi_ex""",con=connection)
+kosdaq = pd.read_sql("""select * from kosdaq_ex""",con=connection)
+rebalancing_date = pd.read_sql("""select * from rebalancing_date""",con=connection)
+month_date = pd.read_sql("""select * from month_date""",con=connection)
+wics_mid = pd.read_sql("""select * from wics_mid""",con=connection)
+
+raw_data = pd.concat([kospi,kosdaq],axis=0,ignore_index=True).drop_duplicates()   #왜인지 모르겠는데 db에 중복된 정보가 들어가있네 ..? 
+col_length = len(rebalancing_date)-1 #rebalancing_date의 길이는 66이다. range로 이렇게 하면 0부터 65까지 66개의 i 가 만들어진다. -1을 해준건 실제 수익률은 -1개가 생성되기 때문.
+
+return_data = pd.DataFrame(np.zeros((1,col_length)))
+data_name = pd.DataFrame(np.zeros((200,col_length)))
+return_month_data = pd.DataFrame(np.zeros((1,3*col_length)))
+quarter_data = pd.DataFrame(np.zeros((200,3*col_length)))
+return_final = pd.DataFrame(np.zeros((1,1)))
+return_month_data = pd.DataFrame(np.zeros((1,3*col_length)))
+
+first_column = len(raw_data.columns)  # 1/pbr 의 loc
+raw_data['1/pbr']=raw_data['EQUITY']/raw_data['MARKET_CAP']
+raw_data['1/per']=raw_data['ADJ_NI_12M_FWD']/raw_data['MARKET_CAP']
+raw_data['div_yield']=raw_data['CASH_DIV_COM']/raw_data['MARKET_CAP']
+raw_data['roe']=raw_data['NI']/raw_data['EQUITY']
+raw_data['roa']=raw_data['NI']/raw_data['ASSET']
+final_column = len(raw_data.columns)-1 # roa 의 loc
+
+for i in range(first_column,final_column+1):
+    a=factor_1(raw_data,rebalancing_date,month_date,i)
+    locals()['aaa_{}'.format(i)] =a.per()    
+
+
+import itertools       
+a=list(itertools.combinations(range(first_column,final_column+1), 2))
+
+
+for n in range(col_length): 
+    first_data = raw_data[raw_data['TRD_DATE']==rebalancing_date.iloc[n,0]] # rebalanging할 날짜에 들어있는 모든 db data를 받아온다.
+    target_data = raw_data[raw_data['TRD_DATE']==rebalancing_date.iloc[n+1,0]] # 다음 리밸런싱 날짜.
+    target_data = target_data.loc[:,['TRD_DATE','GICODE','ADJ_PRC']]
+    # CAP_SIZE : 1코스피대2코스피중3코스피소4코스닥대5코스닥중6코스닥소
+    # 코스닥은 문제가 발생 4 5 6 은 2002년도에 kosdaq big mid small지수가 상장되었기 때문에 그 이전 데이타는 0이라고 나옴
+    # 따라서 iskosdaq라는 새로운 column을 추가했음.
+    # 기존에 했던 backtesting도 틀렸었음.. 물론 2002년 이전 구간만. 그 이후에는 같을것으로 예상
+    first_data = first_data[(first_data['CAP_SIZE']==1)|(first_data['CAP_SIZE']==2)|(first_data['CAP_SIZE']==3)|(first_data['ISKOSDAQ']=='KOSDAQ')]
+    first_data = first_data[first_data['MARKET_CAP']>100000000000]
+    first_data['size_FIF_wisefn'] = first_data['JISU_STOCK']*first_data['FIF_RATIO']*first_data['ADJ_PRC']
+    samsung = first_data[first_data['CO_NM']=='삼성전자']
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

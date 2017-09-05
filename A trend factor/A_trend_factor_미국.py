9dfe823dae17e643572b766a555f7fd0dd3498ea
lshlsh135 <lshlsh135@naver.com>
2017-09-05 09:52:00 +0900

    db 구축시에 호텔_레스토랑_레저 이것을 로텔_레스토랑_레저로 해서 데이터 값이 이상했었음....

--- A trend factor/A_trend_factor_미국.py
+++ A trend factor/A_trend_factor_미국.py
@@ -5,9 +5,12 @@ Created on Tue Aug 22 21:33:25 2017
 @author: LSH-I7-4790
 """
 
+# =============================================================================
+# 12개월 모멘텀 (직전 1개월 제외)
+# =============================================================================
 import pandas as pd
 import cx_Oracle
-
+import numpy as np
 
 #이거 두개 반드시 선언!
 cx0=cx_Oracle.makedsn("localhost",1521,"xe")
@@ -15,6 +18,108 @@ connection = cx_Oracle.connect("lshlsh135","2tkdgns2",cx0)
 
 
 #DATA를 가져온다!!
-america_stock_return = pd.read_sql("""select * from america_stock_return""",con=connection)
+america_stock_return = pd.read_sql("""select * from america_stock_return where SHRCD = '10' or SHRCD = '11'""",con=connection)
+monthly_rebalancing_date = pd.read_sql("""select distinct TRD_DATE from america_stock_return order by TRD_DATE""",con=connection)
+col_length = len(monthly_rebalancing_date)-1
 
-aa=america_stock_return[america_stock_return['TRD_DATE']=='19300503']
+
+return_data = pd.DataFrame(np.zeros((5,col_length-12)))
+america_stock_return['MONTHLY_GROSS_RETURN'] = america_stock_return['RET'] + 1
+a=america_stock_return.head(500)
+
+for n in range(12,col_length): 
+
+    #wics mid sector Momentum 전략을 먼저
+    first_mom = america_stock_return[america_stock_return['TRD_DATE']==monthly_rebalancing_date.iloc[n,0]]
+    last_mom = america_stock_return[america_stock_return['TRD_DATE']==monthly_rebalancing_date.iloc[n-11,0]]
+    target_mom = america_stock_return[america_stock_return['TRD_DATE']==monthly_rebalancing_date.iloc[n+1,0]].loc[:,['PERMNO','MONTHLY_GROSS_RETURN']]
+    last_mom['11M_GROSS_RETURN'] = last_mom.loc[:,'MONTHLY_GROSS_RETURN']
+    for i in range(n-10,n): # 직전 1개월 제외한 12개월치 누적수익률구하기
+        monyhly_date= america_stock_return[america_stock_return['TRD_DATE']==monthly_rebalancing_date.iloc[i,0]].loc[:,['PERMNO','MONTHLY_GROSS_RETURN']]
+        last_mom = pd.merge(last_mom,monyhly_date,on='PERMNO')
+        last_mom['11M_GROSS_RETURN'] = last_mom['11M_GROSS_RETURN'] * last_mom['MONTHLY_GROSS_RETURN_y']
+        last_mom.drop('MONTHLY_GROSS_RETURN_y',axis=1,inplace=True)
+        last_mom.rename(columns={'MONTHLY_GROSS_RETURN_x':'MONTHLY_GROSS_RETURN'},inplace=True)
+        
+    
+    last_mom = last_mom[last_mom['11M_GROSS_RETURN'].notnull()]
+    last_mom = pd.merge(last_mom,target_mom,on='PERMNO')
+    last_mom = last_mom[last_mom['MONTHLY_GROSS_RETURN_y'].notnull()]
+    data_size = len(last_mom)
+    last_mom=last_mom.assign(rnk=np.floor(last_mom['11M_GROSS_RETURN'].rank(method='first')/(data_size/5+1/5))) 
+        
+    
+    data_1=last_mom.query('5>rnk>3')   # 4
+    data_2=last_mom.query('4>rnk>2')   # 3
+    data_3=last_mom.query('3>rnk>1')   # 2
+    data_4=last_mom.query('2>rnk>0')   # 1
+    data_5=last_mom.query('1>rnk>-1')  # 0    
+     
+    return_data.iloc[0,n-12]=np.mean(data_1['MONTHLY_GROSS_RETURN_y'])    # 각각  누적수익률 기록
+    return_data.iloc[1,n-12]=np.mean(data_2['MONTHLY_GROSS_RETURN_y'])
+    return_data.iloc[2,n-12]=np.mean(data_3['MONTHLY_GROSS_RETURN_y'])
+    return_data.iloc[3,n-12]=np.mean(data_4['MONTHLY_GROSS_RETURN_y'])
+    return_data.iloc[4,n-12]=np.mean(data_5['MONTHLY_GROSS_RETURN_y'])
+
+    return_data = return_data.iloc[:,:1079]
+    return_result = np.product(return_data,axis=1)
+
+
+# =============================================================================
+# 12개월 모멘텀 (직전 1개월 포함)
+# =============================================================================
+import pandas as pd
+import cx_Oracle
+import numpy as np
+
+#이거 두개 반드시 선언!
+cx0=cx_Oracle.makedsn("localhost",1521,"xe")
+connection = cx_Oracle.connect("lshlsh135","2tkdgns2",cx0)
+
+
+#DATA를 가져온다!!
+america_stock_return = pd.read_sql("""select * from america_stock_return where SHRCD = '10' or SHRCD = '11'""",con=connection)
+monthly_rebalancing_date = pd.read_sql("""select distinct TRD_DATE from america_stock_return order by TRD_DATE""",con=connection)
+col_length = len(monthly_rebalancing_date)-1
+
+
+return_data = pd.DataFrame(np.zeros((5,col_length-12)))
+america_stock_return['MONTHLY_GROSS_RETURN'] = america_stock_return['RET'] + 1
+a=america_stock_return.head(500)
+
+for n in range(12,col_length): 
+
+    #wics mid sector Momentum 전략을 먼저
+    first_mom = america_stock_return[america_stock_return['TRD_DATE']==monthly_rebalancing_date.iloc[n,0]]
+    last_mom = america_stock_return[america_stock_return['TRD_DATE']==monthly_rebalancing_date.iloc[n-11,0]]
+    target_mom = america_stock_return[america_stock_return['TRD_DATE']==monthly_rebalancing_date.iloc[n+1,0]].loc[:,['PERMNO','MONTHLY_GROSS_RETURN']]
+    last_mom['11M_GROSS_RETURN'] = last_mom.loc[:,'MONTHLY_GROSS_RETURN']
+    for i in range(n-10,n+1): # 직전 1개월 제외한 12개월치 누적수익률구하기
+        monyhly_date= america_stock_return[america_stock_return['TRD_DATE']==monthly_rebalancing_date.iloc[i,0]].loc[:,['PERMNO','MONTHLY_GROSS_RETURN']]
+        last_mom = pd.merge(last_mom,monyhly_date,on='PERMNO')
+        last_mom['11M_GROSS_RETURN'] = last_mom['11M_GROSS_RETURN'] * last_mom['MONTHLY_GROSS_RETURN_y']
+        last_mom.drop('MONTHLY_GROSS_RETURN_y',axis=1,inplace=True)
+        last_mom.rename(columns={'MONTHLY_GROSS_RETURN_x':'MONTHLY_GROSS_RETURN'},inplace=True)
+        
+    
+    last_mom = last_mom[last_mom['11M_GROSS_RETURN'].notnull()]
+    last_mom = pd.merge(last_mom,target_mom,on='PERMNO')
+    last_mom = last_mom[last_mom['MONTHLY_GROSS_RETURN_y'].notnull()]
+    data_size = len(last_mom)
+    last_mom=last_mom.assign(rnk=np.floor(last_mom['11M_GROSS_RETURN'].rank(method='first')/(data_size/5+1/5))) 
+        
+    
+    data_1=last_mom.query('5>rnk>3')   # 4
+    data_2=last_mom.query('4>rnk>2')   # 3
+    data_3=last_mom.query('3>rnk>1')   # 2
+    data_4=last_mom.query('2>rnk>0')   # 1
+    data_5=last_mom.query('1>rnk>-1')  # 0    
+     
+    return_data.iloc[0,n-12]=np.mean(data_1['MONTHLY_GROSS_RETURN_y'])    # 각각  누적수익률 기록
+    return_data.iloc[1,n-12]=np.mean(data_2['MONTHLY_GROSS_RETURN_y'])
+    return_data.iloc[2,n-12]=np.mean(data_3['MONTHLY_GROSS_RETURN_y'])
+    return_data.iloc[3,n-12]=np.mean(data_4['MONTHLY_GROSS_RETURN_y'])
+    return_data.iloc[4,n-12]=np.mean(data_5['MONTHLY_GROSS_RETURN_y'])
+
+    return_data = return_data.iloc[:,:1079]
+    return_result = np.product(return_data,axis=1)
--- Oracle/코스피코스닥_모든팩터조합.py
+++ Oracle/코스피코스닥_모든팩터조합.py
@@ -15,16 +15,18 @@ from factor_3 import factor_3
 from factor_2_mid import factor_2_mid
 from factor_3_mid import factor_3_mid
 from factor_4_mid import factor_4_mid
+from factor_3_mid_대_중소코 import factor_3_mid_대_중소코
+
 
 #이거 두개 반드시 선언!
 cx0=cx_Oracle.makedsn("localhost",1521,"xe")
 connection = cx_Oracle.connect("lshlsh135","2tkdgns2",cx0) #이게 실행이 안될때가 있는데
 #그때는 services에 들어가서 oracle listner를 실행시켜줘야함
 
-kospi_quarter = pd.read_pickle('kospi_quarter')
+kospi_quarter = pd.read_excel("kospi_quarter_data.xlsx",sheetname="kospi",header=None)
 #DATA를 가져온다!!
-kospi = pd.read_sql("""select * from kospi_ex""",con=connection)
-kosdaq = pd.read_sql("""select * from kosdaq_ex""",con=connection)
+kospi = pd.read_sql("""select * from kospi""",con=connection)
+kosdaq = pd.read_sql("""select * from kosdaq""",con=connection)
 rebalancing_date = pd.read_sql("""select * from rebalancing_date""",con=connection)
 month_date = pd.read_sql("""select * from month_date""",con=connection)
 wics_mid = pd.read_sql("""select * from wics_mid""",con=connection)
@@ -49,6 +51,12 @@ raw_data['roa']=raw_data['NI']/raw_data['ASSET']
 raw_data['sales_cap']=raw_data['SALES']/raw_data['MARKET_CAP']
 raw_data['gpro_cap']=raw_data['GROSS_PROFIT']/raw_data['MARKET_CAP']
 raw_data['opro_cap']=raw_data['OPE_PROFIT']/raw_data['MARKET_CAP'] # 이놈 시총제한 있고 없고 차이 심한데 
+raw_data['sales_cap_ttm']=raw_data['SALES_TTM']/raw_data['MARKET_CAP']
+raw_data['opro_cap_ttm']=raw_data['OPE_PROFIT_TTM']/raw_data['MARKET_CAP']
+raw_data['trd_value']=raw_data['TRD_VALUE_60D_MEAN']/raw_data['MARKET_CAP']
+raw_data['vol'] = raw_data['STD_52WEEK']
+raw_data['beta'] = raw_data['BEDA_52WEEK_D']
+
 final_column = len(raw_data.columns)-1 # roa 의 loc
 
 ir_data = pd.DataFrame(np.zeros((final_column-first_column+1,30)))
@@ -83,12 +91,11 @@ for i in range(first_column,final_column+1):
 
 
 
-
-a=factor_2_mid(raw_data,rebalancing_date,month_date,wics_mid,28,29)
-adfadf=a.factor_2_mid()
+a=factor_3_mid(raw_data,rebalancing_date,month_date,wics_mid,32,34,38)
+adfadf=a.factor_3_mid()
 
 #factor 3개 랜덤하게 골라서 성과 측정
-for i in range(first_column,final_column+1):
+for i in range(32,34):
     for j in range(first_column,final_column+1):
         for z in range(first_column,final_column+1):
             if i<j<z:
@@ -108,6 +115,14 @@ for i in range(first_column,final_column+1):
                     locals()['ir_data_{}{}{}{}'.format(i,j,z,p)] = (2*(np.mean(locals()['aaa_{}{}{}{}'.format(i,j,z,p)][1],axis=1)-np.mean(kospi_quarter,axis=1))/np.std(locals()['aaa_{}{}{}{}'.format(i,j,z,p)][1]-kospi_quarter,axis=1))[0]
 
 
+#factor 3개 랜덤하게 골라서 대형주25, 중소코스피 75종목 고름
+for i in range(first_column,final_column+1):
+    for j in range(first_column,final_column+1):
+        for z in range(first_column,final_column+1):
+            if i<j<z:
+                a=factor_3_mid_대_중소코(raw_data,rebalancing_date,month_date,wics_mid,i,j,z)
+                locals()['aaa_{}{}{}'.format(i,j,z)] =a.factor_3_mid_대_중소코()
+                locals()['ir_data_{}{}{}'.format(i,j,z)] = (2*(np.mean(locals()['aaa_{}{}{}'.format(i,j,z)][1],axis=1)-np.mean(kospi_quarter,axis=1))/np.std(locals()['aaa_{}{}{}'.format(i,j,z)][1]-kospi_quarter,axis=1))[0]
 
 
 
